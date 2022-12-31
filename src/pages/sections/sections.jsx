6d2e84d78c6224a129f4a25416c055fec2bc436d
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2022-12-31 20:56:44 +0530

    changed css of alter category

--- src/pages/sections/sections.jsx
+++ src/pages/sections/sections.jsx
@@ -8,35 +8,30 @@ const Sections = ({search}) =>{
 
     // const dataFromContext = useContext(AppContext)
 
-
-    const hairData = sectionData.filter(v=>v.category == "Hair");
-    const faceData = sectionData.filter(v=>v.category == "Face");
-    const threadingData = sectionData.filter(v=>v.category == "Threading");
-    const pediManiData = sectionData.filter(v=>v.category == "Pedicure & Manicure");
-
+    let uniqueCategory = [ ...new Set(sectionData.map(v=>v.category)) ]
 
     return(
         <div className="container-fluid p-0">
         <div className="container-fluid sectionContainer d-flex flex-column">
             <h3>Category</h3>
             <div className="d-flex sectionContainerInner justify-content-lg-center justify-content-xl-center">
-                <a href="#faceSection" className="setctionContents">
-                    <img src="images/facial_image_logo.jpeg" alt="" />
+                <a href="#Face" className="setctionContents">
+                    <img src="/images/facial_image_logo.jpeg" alt="" />
                 <span>face</span>
                 </a>
 
-                <a href="#threadingSection" className="setctionContents">
-                <img src="images/eyebrow_image.jpeg" alt="" />
+                <a href="#Threading" className="setctionContents">
+                <img src="/images/eyebrow_image.jpeg" alt="" />
                 <span>Threading</span>
                 </a>
 
-                <a href="#pediManiSection" className="setctionContents">
-                <img src="images/nailBrush_image.jpeg" alt="" />
+                <a href="#Pedicure & Manicure" className="setctionContents">
+                <img src="/images/nailBrush_image.jpeg" alt="" />
                 <span>Pedicure & Manicure</span>
                 </a>
 
-                <a href="#hairSection" className="setctionContents">
-                <img src="images/hairCut.jpeg"  alt="" />
+                <a href="#Hair" className="setctionContents">
+                <img src="/images/hairCut.jpeg"  alt="" />
                 <span>Hair</span>
                 </a>
 
@@ -45,12 +40,11 @@ const Sections = ({search}) =>{
 
         </div>
         
-            {search.length > 0 &&  <FaceSection id={"searched"} data={search}/> }
+            {/* {search.length > 0 &&  <FaceSection id={"searched"} data={search}/> } */}
             
-           {search.length <= 0 && faceData.length > 0 &&  <FaceSection id={"faceSection"} data={faceData}/> }
-           {search.length <= 0 && hairData.length > 0 &&  <FaceSection id={"hairSection"} data={hairData}/> }
-           {search.length <= 0 && threadingData.length > 0 &&  <FaceSection id={"threadingSection"} data={threadingData}/> }
-           {search.length <= 0 && pediManiData.length > 0 &&  <FaceSection id={"pediManiSection"} data={pediManiData}/> }
+           {search.length <= 0  && uniqueCategory.map((v,i)=>{
+                return <FaceSection id={v} key={i} index={i} data={sectionData.filter((vv, ii)=> vv.category == v)} />
+           })}
 
         </div>
     )
