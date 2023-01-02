79287840791e4cfb5bc8c08f1def611713a34f82
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2023-01-02 17:03:17 +0530

    home banner look changed, navLogo changed, search bar's css changed

--- src/components/atoms/NavLogo.jsx
+++ src/components/atoms/NavLogo.jsx
@@ -3,9 +3,9 @@ import { NavLink } from "react-router-dom"
 
 const NavLogo = () => {
     return (
-        <NavLink to='/' style={{ display:"flex", justifyContent:"center", alignItems:"center"}}>
+        <NavLink to='/' className="navlink" style={{ display:"flex", justifyContent:"center", alignItems:"center"}}>
         <div className="logo" style={{ width: "60px", height: "45px", padding: "0", cursor: "pointer" }}>
-            <img src="/images/logo2.jpeg" style={{ width: "100%", height: "100%" }} alt="" />
+            <img src="/images/navLogo2.jpeg" style={{ width: "100%", height: "100%" }} alt="" />
         </div>
         </NavLink>
 
--- src/components/atoms/search_bar.jsx
+++ src/components/atoms/search_bar.jsx
@@ -29,9 +29,9 @@ const SearchBar = ({searchData}) => {
 
     return (
         <div className="input-group input-group-sm w-100" style={{ minWidth: "180px", maxHeight: "35px", maxWidth: "200px" }}>
-            <input onChange={searchInputFunction} className="form-control" defaultValue="" type="search" placeholder="Search..." name="search" id="" />
-            <span onClick={searchSubmitHandler} className="input-group-text btn btn-primary py-0 d-flex justify-content-center align-items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+            <input style={{background:"transparent", border:"none", borderBottom:"1px solid grey", borderRadius:"0"}} onChange={searchInputFunction} className="form-control" defaultValue="" type="search" placeholder="Search..." name="search" id="" />
+            <span style={{background:"transparent", border:"none", borderBottom:"1px solid grey", borderRadius:"0"}} onClick={searchSubmitHandler} className="input-group-text btn btn-primary py-0 d-flex justify-content-center align-items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="black" className="bi bi-search" viewBox="0 0 16 16">
                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                 </svg>
             </span>
