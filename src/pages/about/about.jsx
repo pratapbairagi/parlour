834c4ee67da123c1e89bdd2490551a6e7e9e44f2
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2023-01-02 17:36:07 +0530

    section's card border text and heading color changed

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -2,7 +2,7 @@
 const About = () => {
     return (
         <div className="container-fluid pb-5">
-            <h2 className="my-2 mx-auto" style={{width:"max-content", padding:"2px 8px", color:"red", fontWeight:"600"}}>About</h2>
+            <h2 className="my-2 mx-auto" style={{width:"max-content", padding:"2px 8px", color:"rgb(215, 4, 115)", fontWeight:"600"}}>About</h2>
             <div className="row d-flex flex-wrap justify-content-center">
 
                 <div className="col col-12 col-md-10 col-lg-5 col-xl-4 p-1" style={{ aspectRatio: "1/.96", boxShadow: "0 0 2px grey" }}>
@@ -11,7 +11,7 @@ const About = () => {
                 </div>
 
                 <div className="col col-12 col-md-10 col-lg-5 col-xl-4 p-3" style={{ background: "aliceblue", boxShadow: "0 0 2px grey", aspectRatio:"1/.9" }}>
-                    <h6 className="m-auto" style={{ borderBottom: "2px solid pink", color: "red", width: "max-content", minWidth: "100px", textAlign: "center", fontSize: "120%", fontWeight: "600" }}>Services</h6>
+                    <h6 className="m-auto" style={{ borderBottom: "2px solid rgb(215, 4, 115)", color: "rgb(215, 4, 115)", width: "max-content", minWidth: "100px", textAlign: "center", fontSize: "120%", fontWeight: "600" }}>Services</h6>
                     <p className="mt-3" style={{ width: "100%", color: "green", fontSize: "90%", fontWeight: "500" }}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium earum eligendi aut, laborum nulla impedit ab, incidunt molestias nihil et nobis esse tempora temporibus vero facilis tenetur!</p>
                     <ol className="mt-2" style={{ width: "80%", color: "grey", fontWeight: "500", fontSize: "80%" }}>
                         <li> Lorem ipsum dolor sit amet consectetur.</li>
@@ -19,41 +19,40 @@ const About = () => {
                         <li> Lorem ipsum dolor sit. </li>
                         <li> Lorem ipsum dolor sit amet. </li>
                     </ol>
-                    <h6 className="mt-4" style={{ color: "red", fontWeight: "500", borderLeft: "2px solid red", padding: "2px 8px" }}>Features</h6>
+                    <h6 className="mt-4" style={{ color: "rgb(215, 4, 115)", fontWeight: "500", borderLeft: "2px solid rgb(215, 4, 115)", padding: "2px 8px" }}>Features</h6>
                     <ol className="mt-2" style={{ display: "flex", flexWrap: "wrap", gap: "6px" }}>
-                        <li style={{ display: "flex", flexDirection: "row", gap: "4px", alignItems: "center", width: "max-content", padding: "3px 9px", border: "2px solid pink", borderRadius: "3px" }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="pink" className="bi bi-calendar-date" viewBox="0 0 16 16">
+                        <li style={{ display: "flex", flexDirection: "row", gap: "4px", alignItems: "center", width: "max-content", padding: "3px 9px", border: "2px solid rgb(215, 4, 115)", borderRadius: "3px" }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="rgb(215, 4, 115)" className="bi bi-calendar-date" viewBox="0 0 16 16">
                                 <path d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z" />
                                 <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
                             </svg>
-                            <span style={{ fontSize: "70%", color: "pink", fontWeight: "600" }}>Book your day</span>
+                            <span style={{ fontSize: "70%", color: "rgb(215, 4, 115)", fontWeight: "600" }}>Book your day</span>
                         </li>
 
-                        <li style={{ display: "flex", flexDirection: "row", gap: "4px", alignItems: "center", width: "max-content", padding: "3px 9px", border: "2px solid pink", borderRadius: "3px" }}>
+                        <li style={{ display: "flex", flexDirection: "row", gap: "4px", alignItems: "center", width: "max-content", padding: "3px 9px", border: "2px solid rgb(215, 4, 115)", borderRadius: "3px" }}>
                             <img src="images/ayurvedic-bowl-svgrepo-com.svg" style={{ width: "24px", height: "24px" }} alt="" />
-                            <span style={{ fontSize: "70%", color: "pink", fontWeight: "600" }}>Herbal Products</span>
+                            <span style={{ fontSize: "70%", color: "rgb(215, 4, 115)", fontWeight: "600" }}>Herbal Products</span>
                         </li>
 
-                        <li style={{ display: "flex", flexDirection: "row", gap: "4px", alignItems: "center", width: "max-content", padding: "3px 9px", border: "2px solid pink", borderRadius: "3px" }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="pink" className="bi bi-clock" viewBox="0 0 16 16">
+                        <li style={{ display: "flex", flexDirection: "row", gap: "4px", alignItems: "center", width: "max-content", padding: "3px 9px", border: "2px solid rgb(215, 4, 115)", borderRadius: "3px" }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="rgb(215, 4, 115)" className="bi bi-clock" viewBox="0 0 16 16">
                                 <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
                                 <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
                             </svg>
-                            <span style={{ fontSize: "70%", color: "pink", fontWeight: "600" }}>10 hrs service</span>
+                            <span style={{ fontSize: "70%", color: "rgb(215, 4, 115)", fontWeight: "600" }}>10 hrs service</span>
                         </li>
 
-                        <li style={{ display: "flex", flexDirection: "row", gap: "4px", alignItems: "center", width: "max-content", padding: "3px 9px", border: "2px solid pink", borderRadius: "3px" }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="pink" className="bi bi-currency-rupee" viewBox="0 0 16 16">
+                        <li style={{ display: "flex", flexDirection: "row", gap: "4px", alignItems: "center", width: "max-content", padding: "3px 9px", border: "2px solid rgb(215, 4, 115)", borderRadius: "3px" }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="rgb(215, 4, 115)" className="bi bi-currency-rupee" viewBox="0 0 16 16">
                                 <path d="M4 3.06h2.726c1.22 0 2.12.575 2.325 1.724H4v1.051h5.051C8.855 7.001 8 7.558 6.788 7.558H4v1.317L8.437 14h2.11L6.095 8.884h.855c2.316-.018 3.465-1.476 3.688-3.049H12V4.784h-1.345c-.08-.778-.357-1.335-.793-1.732H12V2H4v1.06Z" />
                             </svg>
-                            <span style={{ fontSize: "70%", color: "pink", fontWeight: "600" }}>Minmum Charge</span>
+                            <span style={{ fontSize: "70%", color: "rgb(215, 4, 115)", fontWeight: "600" }}>Minmum Charge</span>
                         </li>
                     </ol>
                 </div>
 
                 <div className="col col-12 col-md-10 col-lg-10 col-xl-10" style={{ background: "rgba(0,0,0, .2)", boxShadow: "0 0 2px grey", height: "100%" }}>
                     <h6 className="m-auto" style={{ borderBottom: "2px solid pink", color: "white", width: "max-content", minWidth: "100px", textAlign: "center", fontSize: "120%", fontWeight: "600" }}>History</h6>
-
                 </div>
 
             </div>
--- src/pages/sections/faceSection.jsx
+++ src/pages/sections/faceSection.jsx
@@ -5,12 +5,12 @@ const FaceSection = ({ id, data, index}) => {
     const params = useLocation()
 
     return (<div id={id} className={`container-fluid faceSection_container ${index === 0 && "mt-2"} `} style={{ padding: "8px 4px 10px 4px", background:`${ index !=  "undefined" && index % 2 == 0 ? "aliceblue" :" white" }`}}>
-       {data.length > 0 && <h4 className="mt-1 mb-1" style={{ width: "100%", borderLeft:"2px solid pink", fontSize: "150%", padding: "2px 6px", color: "pink", fontWeight: "600", letterSpacing: "1px" }}>{data[0].category}</h4> }
+       {data.length > 0 && <h4 className="mt-1 mb-1" style={{ width: "100%", borderLeft:"2px solid rgb(203,4,115)", fontSize: "150%", padding: "2px 6px", color: "rgb(203,4,115)", fontWeight: "600", letterSpacing: "1px" }}>{data[0].category}</h4> }
         <div className={`container-fluid ${params.pathname == "/section" ? "d-block" : "d-flex"}`} style={{ overflow: "scroll", padding:"0", margin:"0" }}>
             {data.length > 0 && data.map((v, i) => {
                 return <div key={i} className="row d-flex flex-wrap mt-0" style={{ background: `${ params.pathname == "/section" ? i % 2 === 0 ? "white" : params.pathname == "/section" ? "aliceblue" : "white" : index % 2 == 0 ? "aliceblue" : "white"}`, padding: "12px 4px", display: "flex", justifyContent: "center", minWidth: "240px", width: `${params.pathname == "/section" ? "100%" : "100%"}` }}>
-                    <NavLink  to={`/view/${v.id}`} className={`col ${params.pathname == "/section" && "col-12 col-md-10 col-lg-6 col-xl-5 p-2"} order-1 order-md-1  ${i % 2 === 0 ? "order-lg-2 order-xl-2" : "order-lg-1 order-xl-1"}`} style={{ border: "2px solid pink", padding:"5px", position:"relative" }}>
-                        <span style={{ padding: "2px 8px",  letterSpacing:"1px", background: "pink", fontWeight: "600", color: "white", position:"absolute" }}>{v.title}</span>
+                    <NavLink  to={`/view/${v.id}`} className={`col ${params.pathname == "/section" && "col-12 col-md-10 col-lg-6 col-xl-5 p-2"} order-1 order-md-1  ${i % 2 === 0 ? "order-lg-2 order-xl-2" : "order-lg-1 order-xl-1"}`} style={{ border: "2px solid rgb(203,4,115)", padding:"5px", position:"relative" }}>
+                        <span style={{ padding: "2px 8px",  letterSpacing:"1px", background: "rgb(203,4,115)", fontWeight: "600", color: "white", position:"absolute" }}>{v.title}</span>
                         <img src={v.image} style={{ width: "100%", aspectRaio: "1/1" }} alt="" />
                     </NavLink>
 
@@ -18,7 +18,6 @@ const FaceSection = ({ id, data, index}) => {
                         <div className="card_container" style={{ borderRadius: "2px 24px 2px 24px", height: "100%", backgroundColor: "transparent", padding: "12px 6px", display: "flex", width: "100%", flexDirection: "column", gap: "3px" }}>
                             <h4 style={{ width: "100%", minWidth:"100%", color: `${i % 2 === 0 ? "grey" : "teal"}`, letterSpacing: "1px", maxWidth: "max-content", padding: "2px 8px", alignSelf: `${i % 2 === 0 ? "start" : "start"}`, fontSize: "170%", fontWeight: "600", borderRight: `${i % 2 === 0 ? "none" : "none"}`, borderLeft: `${i % 2 === 0 ? "2px solid red" : "2px solid red"}` }}> 
                             <span>{v.title}</span>
-                            
                             </h4>
 
                             <h6 style={{ width: "100%", textAlign: "left", padding: "2px 16px", background: `${i % 2 === 0 ? "aliceblue" : "white"}`, fontWeight: "500", color: `${i % 2 === 0 ? "grey" : "grey"}`, borderRadius: "4px" }}>Ingredients</h6>
